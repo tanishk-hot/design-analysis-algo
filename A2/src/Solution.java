40796fa31adb81f594d516541a66501fbdae5d28
tanishk-hot <academics.tanishkhot@gmail.com>
2024-08-28 13:10:02 +0530

    BellmanFord

--- A2/src/Solution.java
+++ A2/src/Solution.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
-    public int longestConsecutive(int[] nums) {
+    public static void main(String[] args) {
+        int[] arr = {1,2,0,1};
+        //expected output = 3
+        System.out.println(longest(arr));
+    }
+
+    public static int longestConsecutive(int[] nums) {
         int max = 0;
         int count = 1;
         Arrays.sort(nums);
@@ -17,6 +25,32 @@ class Solution {
                 max = count;
             }
         }
+        return max;
+    }
+    public static int longest(int[] nums){
+        int max = 0;
+
+        if(nums.length  <= 1){
+            return nums.length;
+        }
+        Arrays.sort(nums);
+
+        int j = 1;
+        int count = 1;
+        while(j < nums.length) {
+            if (nums[j] == nums[j-1]) {
+                j++;
+                continue;
+            }
+            if (nums[j-1] + 1 == nums[j]) {
+                count++;
+            }else{
+                max = Math.max(max, count);
+                count =1;
+            }
+            j++;
+        }
+        max = Math.max(max, count);
         return max;
     }
 }
